d25213a76965b69ddea963864d42e784ed56d973
Harish Singh <harishpoctor@gmail.com>
2023-10-14 20:05:48 +0530

    Firebase : task_6_for_dashboard : created

---
+++ Firebase/task_6_for_dashboard/src/components/App.js
@@ -0,0 +1,22 @@
+import { Container } from "react-bootstrap";
+import Signup from "./Signup";
+import { AuthProvider } from "../contexts/AuthContext";
+import { RouterProvider } from "react-router-dom";
+import router from "../routing/routes";
+
+const App = () => {
+  return (
+    <AuthProvider>
+      <Container
+        className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <RouterProvider router={router} />
+        </div>
+      </Container>
+    </AuthProvider>
+  );
+};
+
+export default App;
